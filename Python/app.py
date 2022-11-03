7216876b6c0464ea8c00b4bae3666f0df6d117a3
thecornelisinc <thecornelisinc@gmail.com>
2022-11-02 21:25:52 -0500

    add python folder

---
+++ Python/app.py
@@ -0,0 +1,49 @@
+# Datatype
+# ============
+
+# 1  String - These are text in a single or double column e.g "Good Morning Vivien",
+# print("good morning vivien".title())
+# print("good morning vivien".upper())
+# print("good morning vivien".lower())
+# print(" good morning vivien ".count("o"))
+# print(len((" good morning vivien ").strip()))
+
+# 2. number
+    # operators
+     #  addition 
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(4 / 2)
+# advance
+# print(2**2)
+# print(13//2)
+# print(10%2)
+
+
+# 3. float
+# 
+# number with decimal point
+# print(round(3.14597649464, 2))
+
+# 4. boolean    true or false
+# Are you married ? true or false
+
+
+## WORKING WITH VARIABLES
+# ==============================
+# variable is a container use to store a value
+
+# message = "When a program contains a significant error, Python displays a traceback, which is an error report. Python looks through the file and tries to identify the problem"
+# print(message)
+# add_number = 4+ 6
+# add_more_number = 9
+# print(add_number + add_more_number)
+
+
+# DATA STRUCTURE
+# =============
+                                             
+hobbie = ["orange", "n" , "apple", "grape", 80, True, ["boy", "girl"]]
+question = "What is the first hobby of kola"
+print(hobbie[6][0])
---
+++ Python/main.py
@@ -0,0 +1,66 @@
+# SECTION 1
+# ============================================
+
+# Python Installation 3.7 from https://pyhton.org
+# Python Basic
+    # How to check Python version  "python --version" 
+    # python --version
+    # How to Run Python code in a terminal
+    # How to run python code from a text editor
+    # How to wrap too long text with "alt + z"
+    # how to save python file ctl + s
+    # prevent python from running a line of code(comment)
+# Run Hello World!
+
+# How to Troubleshoot Python code
+    # When a program contains a significant error, Python displays a traceback, which is an error report. Python looks through the file and tries to identify the problem. Check the traceback; it might give you a clue as to what issue is preventing the program from running.
+
+# Step away from your computer, take a short break, and then try again. Remember that syntax is very important in programming, so even a missing colon, a mismatched quotation mark, or mismatched parentheses can prevent a program from running properly. 
+
+# Start over again. You probably don’t need to uninstall any software, but it might make sense to delete your file-name.py file and re-create it from scratch
+
+# Ask your pair/someone else review your code, and watch what they do carefully. You might have missed one small step that someone else happens to catch.
+
+# Google-fu is your friend
+
+
+# SECTION 2
+# ===================================
+
+# Variables and Simple Data type
+# ================================
+# 1. String (can use either single or double quote)
+    # title (title())
+    # upercase (upper())
+    # lowercase (lower())
+    # format (using variable in string )
+    # stripping whitespace(lstrip,rstrip, strip)
+# 2. Number
+#       operators(+,-,/,*,**,//,%,=,==)
+# 3. Floats
+# 4. bool  (true or false)
+# 5. List  (square bracket indicate list)
+    # accessing items in list
+    # Changing, Adding, and Removing Elements
+    # stating with an empty list the add items to the list
+    # using individual item in a list
+
+
+num = [1,2,4]
+num.insert(2, 3)
+print(num)
+
+
+# VARIABLE RULES
+
+    # Variable names can contain only letters, numbers, and underscores. They can start with a letter or an underscore, but not with a number. For instance, you can call a variable message_1 but not 1_message.
+
+    # Spaces are not allowed in variable names, but underscores can be used to separate words in variable names. For example, greeting_message works, but greeting message will cause errors.
+
+    # Avoid using Python keywords and function names as variable names; that is, do not use words that Python has reserved for a particular programmatic purpose, such as the word print. 
+
+    # Variable names should be short but descriptive. For example, name is better than n, student_name is better than s_n, and name_length is better than length_of_persons_name.
+
+    # Be careful when using the lowercase letter l and the uppercase letter O because they could be confused with the numbers 1 and 0.
+
+
